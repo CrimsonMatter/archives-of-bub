d81f64720096a4ea38a8b9b07a6cebb8c616d879
Illuminatus1111 <crimsonmattergames@gmail.com>
2026-01-28 19:38:58 -0500

    Sitemap
    >Update sitemap with actual URL.
    >Update robots with actual URL.
    >Update various files with actual URL.

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,9 +1,9 @@
 
 export default new Map([
-["src/content/library/cats.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flibrary%2Fcats.mdx&astroContentModuleFlag=true")],
 ["src/content/library/electricity.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flibrary%2Felectricity.mdx&astroContentModuleFlag=true")],
 ["src/content/library/gravity.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flibrary%2Fgravity.mdx&astroContentModuleFlag=true")],
-["src/content/library/theseus.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flibrary%2Ftheseus.mdx&astroContentModuleFlag=true")],
+["src/content/library/cats.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flibrary%2Fcats.mdx&astroContentModuleFlag=true")],
 ["src/content/library/observation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flibrary%2Fobservation.mdx&astroContentModuleFlag=true")],
-["src/content/library/time.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flibrary%2Ftime.mdx&astroContentModuleFlag=true")]]);
+["src/content/library/time.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flibrary%2Ftime.mdx&astroContentModuleFlag=true")],
+["src/content/library/theseus.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flibrary%2Ftheseus.mdx&astroContentModuleFlag=true")]]);
 		
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://YOUR-PROJECT.pages.dev",
+  site: "https://archives-of-bub.pages.dev",
   integrations: [
     tailwind(),
     mdx(),
